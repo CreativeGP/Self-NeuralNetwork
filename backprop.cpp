1a083dd720df81bfbca20e55ce43c54693b551c6
CreativeGP <malicktafaminecraft@gmail.com>
2018-05-30 22:25:29 +0900

    Fixed Algorithm

--- backprop.cpp
+++ backprop.cpp
@@ -84,6 +84,14 @@ void show_network (vector<vector<Neuron>> &net) {
     }
 }
 
+void set_input(vector<vector<Neuron>> &net, vector<double> inputs) {
+    if (net[0].size() != inputs.size()) return;
+
+    for (int i = 0; i < inputs.size(); ++i) {
+        net[0][i].in = net[0][i].value = inputs[i];
+    }
+}
+
 void dense(int layer_num, vector<vector<Neuron>> *net) {
 
     int back_layer_num = net->back().size();
@@ -107,12 +115,12 @@ void dense(int layer_num, vector<vector<Neuron>> *net) {
         (*prev(net->end(), 2))[i].nexts = this_layer;
 }
 
-double calculate_value(Neuron& n) {
+double calculate_value(Neuron& n, bool forcibly = false) {
     if (n.backs.size() != 0) {
         vector<double> values;
         for (auto e : n.backs) {
             // Don't calculate already calculated value.
-            if (e->value == 0) calculate_value(*e);
+            if (e->value == 0 && !forcibly) calculate_value(*e);
             values.push_back(e->value);
         }
 
@@ -126,9 +134,9 @@ double calculate_value(Neuron& n) {
     }
 }
 
-void update_network(vector<vector<Neuron>>& net) {
+void update_network(vector<vector<Neuron>>& net, bool forcibly = false) {
     for (auto &e : net.back())
-        calculate_value(e);
+        calculate_value(e, forcibly);
 }
 
 double error(vector<vector<Neuron>>& net, vector<double> teacher) {
@@ -150,6 +158,7 @@ double error(vector<vector<Neuron>>& net, vector<double> teacher) {
 #define object error
 
 double LEARNING_RATE = 1;
+double OBJECT_ACC = 0.00001;
 
 void fit(vector<vector<Neuron>>& net, vector<double> teacher) {
     vector<double> last_error_by_in;
@@ -158,21 +167,17 @@ void fit(vector<vector<Neuron>>& net, vector<double> teacher) {
             double error_by_out;
             if (i == net.size()-1)
                 error_by_out = net[i][j].value - teacher[j];
+            else {
+                for (int e = 0; e < net[i+1].size(); ++e)
+                    error_by_out += last_error_by_in[e] * net[i+1][e].weights[j];
+            }
             
             double out_by_in = d_s(net[i][j].in);
-            if (last_error_by_in.size() > 0) last_error_by_in.clear();
+            last_error_by_in.clear();
             last_error_by_in.push_back(error_by_out * out_by_in);
             
             double in_by_weights;
             for (int k = 0; k < net[i][j].weights.size(); ++k) {
-                if (i != net.size()-1) {
-                    error_by_out = 0;
-                    for (int l = 0; l < net[i+1].size(); ++l) {
-//                        cout << last_error_by_in[l]*net[i+1][l].weights[j] << endl;;
-                        error_by_out += last_error_by_in[l] * net[i+1][l].weights[j];
-//                        cout << i << " " << j << " " << k  << " " << l << endl;
-                    }
-                }
                 in_by_weights = net[i-1][k].value;
                 net[i][j].weights[k] -= LEARNING_RATE * error_by_out * out_by_in * in_by_weights;
             }
@@ -206,20 +211,39 @@ int main() {
         }
     };
 
+
+    dense(4, &network);
+    dense(4, &network);
+
     vector<double> teacher = {0.3, 0.8};
+//    set_input(network, {0.1, 0.9});
 
-    dense(3, &network);
-    dense(3, &network);
-    dense(2, &network);
+    cout << "inputs: " << endl;
+    double gtmp; cin >> gtmp;
+    cout << "Network input {" << gtmp <<"}: " << endl;
+    rep(i, gtmp) {
+        double tmp; cin >> tmp;
+        network[0][i].value = tmp;
+    }
 
+
+    cout << "outputs: " << endl;
+    cin >> gtmp;
+    cout << "Want to be {" << gtmp << "}: " << endl;
+    rep(i, gtmp) {
+        double tmp; cin >> tmp;
+        teacher[i] = tmp;
+    }
+
+    dense(gtmp, &network);
 
     cout << error(network, {1, 1}) << endl;
     show_network(network);
 
 
-    while (error(network, teacher) > 0.0001) {
-        if (error(network, teacher) < 0.01) LEARNING_RATE = 0.3;
-        if (error(network, teacher) < 0.001) LEARNING_RATE = 0.01;
+    while (error(network, teacher) > OBJECT_ACC) {
+        // if (error(network, teacher) < 0.01) LEARNING_RATE *= 0.3;
+        // if (error(network, teacher) < 0.001) LEARNING_RATE *= 0.01;
 //        LEARNING_RATE = ;
         
         fit(network, teacher);
@@ -230,6 +254,9 @@ int main() {
         cout << "Error: " << error(network, teacher) << endl;
 //        usleep(1000000 * 0.5);
     }
+
+    update_network(network, true);
+    show_network(network);
              
     return 0;
 }
